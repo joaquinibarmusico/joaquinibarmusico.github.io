d191bc28881e197fc946caef29a65372d2285ffe
joaquinibarmusico <joaquin.ibar@gmail.com>
2016-07-11 16:41:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
+var img;
+function preload() {
+  img = loadImage("assets/fotoportada.jpg");
+}
+
 function setup() {
-
-
+  image(img, 0, 0);
+  image(img, 0, 0, 100, 100);
+  image(img, 0, 0, 100, 100, 0, 0, 100, 100);
 }
 
 function draw() {
   createCanvas(windowWidth, windowHeight);
   background(0, 10, 20);
+  loadImage("assets/laDefense.jpg", function(img) {
+  image(img, 0, 0);
   textSize(windowHeight*0.1);
   textAlign(CENTER);
   fill(0, 102, 153);
@@ -14,3 +22,6 @@ function draw() {
   text("joaquin.ibar@gmail.com", windowWidth / 2, windowHeight * 0.7);
  
 }
+
+
+

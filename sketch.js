b09c0e528cc64c82affd66a1779c2c7f6cfcc1ed
joaquinibarmusico <joaquin.ibar@gmail.com>
2016-07-11 12:41:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 function draw() {
 
   background(0, 10, 20);
-  textSize(windowHeight * 0.1);
+  textSize(windowWidth);
   textAlign(CENTER);
   fill(0, 102, 153);
   text("Joaquín Ibar S.", windowWidth / 2, windowHeight * 0.3);

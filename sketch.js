ce98b1a27250fe870b695c80c7f9a23cc87206bf
joaquinibarmusico <joaquin.ibar@gmail.com>
2016-07-11 09:19:18 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+ background(0, 10, 20);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}

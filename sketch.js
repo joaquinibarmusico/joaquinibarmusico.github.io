4fd18702e5a49cd6bd651abc85082a08bb03be64
joaquinibarmusico <joaquin.ibar@gmail.com>
2016-07-11 12:37:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
- background(0, 10, 20);
-}
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  background(0, 10, 20);
+  textSize(windowHeight * 0.1);
+  textAlign(CENTER);
+  fill(0, 102, 153);
+  text("Joaquín Ibar S.", windowWidth / 2, windowHeight * 0.3);
+  text("https://www.facebook.com/joaquin.ibar", windowWidth / 2, windowHeight * 0.5);
+  text("joaquin.ibar@gmail.com", windowWidth / 2, windowHeight * 0.7);
+  // resizeCanvas(windowWidth, windowHeight);
+  //windowResized();
+
 }
